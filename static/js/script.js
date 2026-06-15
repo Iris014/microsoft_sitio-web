49ae910dedc09e050f13c4256da24972b5c21336
Iris014 <irisccallomamani@liceovvh.cl>
2026-06-15 09:46:19 -0400

    saved

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,21 @@
 console.log("Conexión exitosa")
 
+function cotizarLicenciasWindows() {
+    let empresa = prompt("Ingrese el nombre de su empresa: ");
+    let cantidad = parseInt(prompt("¿Cuántas licencias de Windows necesita?: "));
+    
+    if (cantidad >= 50) {
+        let descuento = (cantidad * 80000) * 0.2;
+        let total = (cantidad * 80000) - descuento;
+        alert("Empresa: " + empresa + "\nDescuento (20%): $" + descuento + "\nTotal: $" + total);
+    } else if (cantidad >= 20) {
+        let descuento = (cantidad * 80000) * 0.1;
+        let total = (cantidad * 80000) - descuento;
+        alert("Empresa: " + empresa + "\nDescuento (10%): $" + descuento + "\nTotal: $" + total);
+    } else if (cantidad < 20 && cantidad > 0) {
+        let total = cantidad * 80000;
+        alert("Empresa: " + empresa + "\nDescuento: 0%\nTotal: $" + total);
+    } else {
+        alert("Ingrese una cantidad válida.");
+    }
+}
